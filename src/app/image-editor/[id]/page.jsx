193b7ad37e869cc1996e89ebc853d54709011856
Rafath-Auvee <rbza98@outlook.com>
2023-06-25 23:30:23 +0600

    input working

--- src/app/image-editor/[id]/page.jsx
+++ src/app/image-editor/[id]/page.jsx
@@ -1,12 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-
 import images from "../../../data/image";
-import TextStyleEditor from "@/components/ImageEditor/TextStyleEditor";
-import ImageCanvas from "@/components/ImageEditor/ImageCanvas";
 
 const ImageEditor = ({ params }) => {
+  const canvasRef = useRef(null);
   const imageData = images.find((image) => image.id === parseInt(params.id));
 
   const [textStyles, setTextStyles] = useState(
@@ -17,11 +15,107 @@ const ImageEditor = ({ params }) => {
   );
   const [selectedTextIndex, setSelectedTextIndex] = useState(null);
 
-  const handleTextStyleChange = (index, property, value) => {
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const image = new Image();
+    image.src = imageData.url;
+
+    image.onload = () => {
+      // Draw the image on the canvas
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      // Loop through the text styles and draw the overlay markers on the canvas
+      textStyles.forEach((textStyle) => {
+        context.fillStyle = textStyle.backgroundColor;
+        context.fillRect(
+          textStyle.left,
+          textStyle.top,
+          textStyle.width,
+          textStyle.height
+        );
+      });
+    };
+  }, [imageData, textStyles]);
+
+  const handleLeftChange = (index, e) => {
     const updatedTextStyles = [...textStyles];
     updatedTextStyles[index] = {
       ...updatedTextStyles[index],
-      [property]: value,
+      left: parseInt(e.target.value),
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const handleTopChange = (index, e) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      top: parseInt(e.target.value),
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const handleFontSizeChange = (index, e) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      fontSize: parseInt(e.target.value),
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const incrementLeft = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      left: updatedTextStyles[index].left + 1,
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const decrementLeft = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      left: updatedTextStyles[index].left - 1,
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const incrementTop = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      top: updatedTextStyles[index].top + 1,
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const decrementTop = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      top: updatedTextStyles[index].top - 1,
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const incrementFontSize = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      fontSize: updatedTextStyles[index].fontSize + 1,
+    };
+    setTextStyles(updatedTextStyles);
+  };
+
+  const decrementFontSize = (index) => {
+    const updatedTextStyles = [...textStyles];
+    updatedTextStyles[index] = {
+      ...updatedTextStyles[index],
+      fontSize: updatedTextStyles[index].fontSize - 1,
     };
     setTextStyles(updatedTextStyles);
   };
@@ -38,18 +132,123 @@ const ImageEditor = ({ params }) => {
       {selectedTextIndex !== null && (
         <p>Selected Text: {textStyles[selectedTextIndex].text}</p>
       )}
-      <ImageCanvas
-        imageData={imageData}
-        textStyles={textStyles}
-        selectedTextIndex={selectedTextIndex}
-        handleTextClick={handleTextClick}
-      />
       {selectedTextIndex !== null && (
-        <TextStyleEditor
-          textStyle={textStyles[selectedTextIndex]}
-          handleTextStyleChange={handleTextStyleChange}
-        />
+        <div className="flex justify-center mt-4">
+          <div
+            key={selectedTextIndex}
+            className={`flex flex-row items-center mx-4 gap-10 px-5 py-2 bg-blue-400 text-blue-800 rounded border-blue-500 border-2`}
+          >
+            <label htmlFor={`leftInput-${selectedTextIndex}`}>
+              Left Position:
+            </label>
+            <input
+              id={`leftInput-${selectedTextIndex}`}
+              type="number"
+              value={textStyles[selectedTextIndex].left}
+              onChange={(e) => handleLeftChange(selectedTextIndex, e)}
+              className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black w-16"
+            />
+            <div className="flex mt-2">
+              <button
+                onClick={() => incrementLeft(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1 mr-1"
+              >
+                +
+              </button>
+              <button
+                onClick={() => decrementLeft(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1"
+              >
+                -
+              </button>
+            </div>
+
+            <label htmlFor={`topInput-${selectedTextIndex}`}>
+              Top Position:
+            </label>
+            <input
+              id={`topInput-${selectedTextIndex}`}
+              type="number"
+              value={textStyles[selectedTextIndex].top}
+              onChange={(e) => handleTopChange(selectedTextIndex, e)}
+              className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black w-16"
+            />
+            <div className="flex mt-2">
+              <button
+                onClick={() => incrementTop(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1 mr-1"
+              >
+                +
+              </button>
+              <button
+                onClick={() => decrementTop(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1"
+              >
+                -
+              </button>
+            </div>
+
+            <label htmlFor={`fontSizeInput-${selectedTextIndex}`}>
+              Font Size:
+            </label>
+            <input
+              id={`fontSizeInput-${selectedTextIndex}`}
+              type="number"
+              value={textStyles[selectedTextIndex].fontSize}
+              onChange={(e) => handleFontSizeChange(selectedTextIndex, e)}
+              className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black w-16"
+            />
+            <div className="flex mt-2">
+              <button
+                onClick={() => incrementFontSize(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1 mr-1"
+              >
+                +
+              </button>
+              <button
+                onClick={() => decrementFontSize(selectedTextIndex)}
+                className="bg-gray-200 rounded px-2 py-1"
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
       )}
+      <div className="relative">
+        <canvas
+          ref={canvasRef}
+          className="border border-gray-500"
+          width={500} // Set the width of the canvas to match your desired image width
+          height={500} // Set the height of the canvas to match your desired image height
+        ></canvas>
+        {textStyles.map((textStyle, index) => (
+          <div
+            key={index}
+            className={`absolute ${
+              selectedTextIndex === index ? "border-blue-500 border-2" : ""
+            }`}
+            style={{
+              left: textStyle.left,
+              top: textStyle.top,
+              cursor: "pointer",
+            }}
+            onClick={() => handleTextClick(index)}
+          >
+            <div
+              style={{
+                // backgroundColor: textStyle.backgroundColor,
+                color: "black",
+                fontFamily: textStyle.fontFamily,
+                fontSize: `${textStyle.fontSize}px`,
+              }}
+            >
+              {textStyle.text}
+            </div>
+          </div>
+        ))}
+      </div>
+
       <div className="flex mt-4">
         <Link href="/image-editor">
           <p className="bg-gray-200 rounded px-4 py-2 mr-2">Back</p>
--- src/components/ImageEditor/TextStyleEditor.jsx
+++ src/components/ImageEditor/TextStyleEditor.jsx
@@ -1,92 +1,60 @@
 const TextStyleEditor = ({ textStyle, handleTextStyleChange }) => {
-  const handleChange = (property, e) => {
-    handleTextStyleChange(textStyle.index, property, e.target.value);
-  };
-
-  const handleIncrement = (property) => {
-    handleTextStyleChange(textStyle.index, property, textStyle[property] + 1);
-  };
-
-  const handleDecrement = (property) => {
-    handleTextStyleChange(textStyle.index, property, textStyle[property] - 1);
-  };
-
-  return (
-    <div className="flex justify-center mt-4">
-      <div
-        className={`flex flex-col items-center mx-4 border-blue-500 border-2`}
-      >
-        <label htmlFor={`leftInput-${textStyle.index}`}>Left Position:</label>
-        <input
-          id={`leftInput-${textStyle.index}`}
-          type="number"
-          value={textStyle.left}
-          onChange={(e) => handleChange("left", e)}
-          className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
-        />
-        <div className="flex mt-2">
-          <button
-            onClick={() => handleIncrement("left")}
-            className="bg-gray-200 rounded px-2 py-1 mr-1"
-          >
-            +
-          </button>
-          <button
-            onClick={() => handleDecrement("left")}
-            className="bg-gray-200 rounded px-2 py-1"
-          >
-            -
-          </button>
-        </div>
-
-        <label htmlFor={`topInput-${textStyle.index}`}>Top Position:</label>
-        <input
-          id={`topInput-${textStyle.index}`}
-          type="number"
-          value={textStyle.top}
-          onChange={(e) => handleChange("top", e)}
-          className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
-        />
-        <div className="flex mt-2">
-          <button
-            onClick={() => handleIncrement("top")}
-            className="bg-gray-200 rounded px-2 py-1 mr-1"
-          >
-            +
-          </button>
-          <button
-            onClick={() => handleDecrement("top")}
-            className="bg-gray-200 rounded px-2 py-1"
-          >
-            -
-          </button>
-        </div>
-
-        <label htmlFor={`fontSizeInput-${textStyle.index}`}>Font Size:</label>
-        <input
-          id={`fontSizeInput-${textStyle.index}`}
-          type="number"
-          value={textStyle.fontSize}
-          onChange={(e) => handleChange("fontSize", e)}
-          className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
-        />
-        <div className="flex mt-2">
-          <button
-            onClick={() => handleIncrement("fontSize")}
-            className="bg-gray-200 rounded px-2 py-1 mr-1"
-          >
-            +
-          </button>
-          <button
-            onClick={() => handleDecrement("fontSize")}
-            className="bg-gray-200 rounded px-2 py-1"
-          >
-            -
-          </button>
+    const handleChange = (property, e) => {
+      handleTextStyleChange(textStyle.index, property, e.target.value);
+    };
+  
+    const handleIncrement = (property) => {
+      handleTextStyleChange(textStyle.index, property, textStyle[property] + 1);
+    };
+  
+    const handleDecrement = (property) => {
+      handleTextStyleChange(textStyle.index, property, textStyle[property] - 1);
+    };
+  
+    return (
+      <div className="flex justify-center mt-4">
+        <div className={`flex flex-col items-center mx-4 border-blue-500 border-2`}>
+          <label htmlFor={`leftInput-${textStyle.index}`}>Left Position:</label>
+          <input
+            id={`leftInput-${textStyle.index}`}
+            type="number"
+            value={textStyle.left}
+            onChange={(e) => handleChange("left", e)}
+            className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
+          />
+          <div className="flex mt-2">
+            <button onClick={() => handleIncrement("left")} className="bg-gray-200 rounded px-2 py-1 mr-1">+</button>
+            <button onClick={() => handleDecrement("left")} className="bg-gray-200 rounded px-2 py-1">-</button>
+          </div>
+  
+          <label htmlFor={`topInput-${textStyle.index}`}>Top Position:</label>
+          <input
+            id={`topInput-${textStyle.index}`}
+            type="number"
+            value={textStyle.top}
+            onChange={(e) => handleChange("top", e)}
+            className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
+          />
+          <div className="flex mt-2">
+            <button onClick={() => handleIncrement("top")} className="bg-gray-200 rounded px-2 py-1 mr-1">+</button>
+            <button onClick={() => handleDecrement("top")} className="bg-gray-200 rounded px-2 py-1">-</button>
+          </div>
+  
+          <label htmlFor={`fontSizeInput-${textStyle.index}`}>Font Size:</label>
+          <input
+            id={`fontSizeInput-${textStyle.index}`}
+            type="number"
+            value={textStyle.fontSize}
+            onChange={(e) => handleChange("fontSize", e)}
+            className="border border-gray-300 rounded px-2 py-1 mt-1 placeholder:text-black"
+          />
+          <div className="flex mt-2">
+            <button onClick={() => handleIncrement("fontSize")} className="bg-gray-200 rounded px-2 py-1 mr-1">+</button>
+            <button onClick={() => handleDecrement("fontSize")} className="bg-gray-200 rounded px-2 py-1">-</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default TextStyleEditor;
+    );
+  };
+  
+  export default TextStyleEditor;
